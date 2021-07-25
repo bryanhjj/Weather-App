1f033cb89f58f2df9d5d5b7c5026d85f784fdfd4
bryanhjj <83968222+bryanhjj@users.noreply.github.com>
2021-07-25 22:38:40 +0800

    basic functionality completed

---
+++ weather.js
@@ -0,0 +1,49 @@
+const apiKey = "e8957761dd3908d129221b5a9d89eae3";
+const form = document.querySelector('#form');
+const userLocation = document.querySelector('#locationForm');
+const searchButton = document.querySelector('#search');
+const resetButton = document.querySelector('#reset');
+const weatherDescrip = document.querySelector('#weatherDescription');
+const temperature = document.querySelector('#temperature');
+const humidity = document.querySelector('#humidity');
+
+
+// updates the relevant DOM elements with the extracted data
+const dataDisplay = (obj) => {
+  weatherDescrip.textContent = obj.description;
+  temperature.textContent = Math.round(obj.cTemp) + "°C / " + Math.round(obj.fTemp) + "°F";
+  humidity.textContent = obj.humidity + "%";
+};
+
+// extracts the data that I want from the returned json object and stores them for later use
+const dataExtract = (rawData) => {
+  const dataStorage = {
+    description : rawData.weather[0].main,
+    cTemp : rawData.main.temp - 273.15,
+    fTemp : (rawData.main.temp - 273.15) * 9 / 5 + 32,
+    humidity : rawData.main.humidity
+  };
+  return dataStorage;
+};
+
+// obtains the weather data according to user input, process said data (dataExtract()) and displays it (dataDisplay())
+const getWeatherData = async () => {
+     let userLocData = userLocation.value;
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userLocData}&appid=${apiKey}`, 
+      {
+        mode: 'cors'
+      });
+      const weatherData = await res.json();
+      const sanitisedData = dataExtract(weatherData);
+      dataDisplay(sanitisedData);
+    } catch(err) {
+      alert(err);
+    };
+};
+
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  getWeatherData();
+  form.reset();
+});
